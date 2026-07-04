e4cad53c5fc4555b3fe1dd610c4bd385e0e97b0d
AULIA YASMIN <itsmeayas12@gmail.com>
2026-07-04 18:35:19 +0700

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { getRole } = require('../controllers/authController');
-
-router.get('/role/:address', getRole); // GET /api/auth/role/:address
-
-module.exports = router;
